d90a2e5a0c5ff1d73d8d305a85e7065ba9b693b3
Rainer Poisel <rainer.poisel@gmail.com>
2013-07-10 22:16:38 +0200

    canonicalization added

---
+++ test/prepare_sign.py
@@ -0,0 +1,24 @@
+
+
+def recurse(pData, pList):
+    lStack = [pData]
+    while len(lStack) is not 0:
+        lObj = lStack.pop()
+        if type(lObj) is dict:
+            for lValue in sorted(lObj.items()):
+                lStack.append(lValue)
+        elif type(lObj) is list or type(lObj) is tuple:
+            for lElem in sorted(lObj):
+                lStack.append(lElem)
+        else:
+            pList.append(lObj)
+
+
+def canonicalize(pData):
+    lElemList = []
+    recurse(pData, lElemList)
+
+    return '\n'.join(lElemList)
+
+
+
---
+++ test/rechash.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+
+import os
+
+from Crypto.Signature import PKCS1_v1_5
+from Crypto.Hash import SHA
+from Crypto.PublicKey import RSA
+
+from prepare_sign import canonicalize
+
+
+def main():
+    # sign
+    message = {
+        'key1': ['value1', 'value3'],
+        'key2': {'some': 'value2', 'thing': 'value4'}
+    }
+
+    print(canonicalize(message))
+
+    key = RSA.importKey(open(os.path.join('..', 'res', 'private.pem')).read())
+    h = SHA.new(canonicalize(message))
+    signer = PKCS1_v1_5.new(key)
+    signature = signer.sign(h)
+
+    # veify
+    message = {
+        'key2': {'some': 'value2', 'thing': 'value4'},
+        'key1': ['value1', 'value3']
+    }
+    key = RSA.importKey(open(os.path.join('..', 'res', 'public.pem')).read())
+    h = SHA.new(canonicalize(message))
+    verifier = PKCS1_v1_5.new(key)
+    if verifier.verify(h, signature):
+        print "The signature is authentic."
+    else:
+        print "The signature is not authentic."
+
+if __name__ == "__main__":
+    main()
