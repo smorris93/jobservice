419d651cd6af12c7a68b1038ddb53ae5f450d533
Rainer Poisel <rainer.poisel@gmail.com>
2013-07-10 20:25:50 +0200

    example files added

---
+++ test/digsig.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+import os
+
+from Crypto.Signature import PKCS1_v1_5
+from Crypto.Hash import SHA
+from Crypto.PublicKey import RSA
+
+
+def main():
+    # sign
+    message = 'To be signed'
+    key = RSA.importKey(open(os.path.join('..', 'res', 'private.pem')).read())
+    h = SHA.new(message)
+    signer = PKCS1_v1_5.new(key)
+    signature = signer.sign(h)
+
+    # veify
+    key = RSA.importKey(open(os.path.join('..', 'res', 'public.pem')).read())
+    h = SHA.new(message)
+    verifier = PKCS1_v1_5.new(key)
+    if verifier.verify(h, signature):
+        print "The signature is authentic."
+    else:
+        print "The signature is not authentic."
+
+if __name__ == "__main__":
+    main()
